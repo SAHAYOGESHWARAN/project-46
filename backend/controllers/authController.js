0132b0f5196b567aecec335ae3320bc550b85ee6
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-24 20:05:40 +0530

    46

---
+++ backend/controllers/authController.js
@@ -0,0 +1,31 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id, role) => {
+  return jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+};
+
+exports.registerUser = async (req, res) => {
+  const { name, email, password, role } = req.body;
+  try {
+    const user = new User({ name, email, password, role });
+    await user.save();
+    res.status(201).json({ token: generateToken(user._id, user.role) });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user && (await user.matchPassword(password))) {
+      res.json({ token: generateToken(user._id, user.role) });
+    } else {
+      res.status(401).json({ message: 'Invalid email or password' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
